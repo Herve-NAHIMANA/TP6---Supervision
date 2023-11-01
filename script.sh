9cf09221d7a7dbd639357cb4d43fe5e24d49c5c6
Herve NAHIMANA <herve.nahimana1996@gmail.com>
2023-11-01 21:45:00 +0100

    Script d'installation

--- script.sh
+++ script.sh
@@ -29,23 +29,3 @@ helm install prometheus prometheus-community/kube-prometheus-stack --namespace m
 # Installation de mysql
 kubectl create secret generic mysecret --from-literal=ROOT_PASSWORD=demo -n monitoring
 kubectl apply -f mysql/mysql-statefulset.yaml
-# Expose le port 9090
-pods_list=($(kubectl --namespace monitoring get pods -l "release=prometheus"))
-target_count=3
-current_count=0
-for line in "${pods_list[@]}"; do
-    pod_name=$(echo "$line" | awk '{print $1}')
-    pod_status=$(echo "$line" | awk '{print $3}')
-    # Exécute la commande 'kubectl get pods' et capture la sortie
-    if [[ "$pod_status" == "Running" ]]; then
-        current_count=$((current_count + 1))
-        echo "Le pod $pod_name est en état 'Running'."
-    if [ "$current_count" -eq "$target_count" ]; then
-        kubectl port-forward --address 0.0.0.0 svc/prometheus-kube-prometheus-prometheus -n monitoring 9090 
-        break
-    fi
-    else
-        echo "Le pod $pod_name est en état 'Pending', en attente..."
-        sleep 5  # Attendre quelques secondes avant de vérifier à nouveau
-    fi
-done
